dfc60bc6cc3689148a8def2fee2a0291f3788a4c
A1aM0 <yoyoyanghao@126.com>
2020-03-25 15:03:28 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+    res.send('Test');
+});
 
 app.listen(3000, function () {
     console.log('Listening -- Sever has started at http://localhost:3000/');

316560fb774231e0b93e09a1d1ccb0a6a281d027
Songhao Yang <yoyoyanghao@126.com>
2020-05-14 15:32:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -139,6 +139,11 @@ app.get('/about', function (req, res) {
     res.render('about');
 });
 
+app.get('/halo', function (req, res) {
+    res.redirect('http://localhost:8090/');
+    console.log('redirect to halo system.');
+}
+
 app.get('/*', function (req, res) {
     res.send('Page you searched is not existed, please go back!');
 });
